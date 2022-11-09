1d057cfb7ef97ca5cec271531f20926eb3b661ee
mikevarriano <113132199+mikevarriano@users.noreply.github.com>
2022-11-09 13:59:03 -0500

    wwwww

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
   point: document.querySelectorAll(".point"),
-  background: document.querySelector("#big-black-box"),
+  box: document.getElementById("#big-black-box"),
 };
 console.log(DOMSelectors.button);
 
 function backgroundAndText(background, text) {
   background.style.backgroundColor = "red";
-  text.innerHTML = "This is now a big red box";
+  text.innerContent = "This is now a big red box";
   text.style.fontSize = "40px";
 }
-backgroundAndText(DOMSelectors.background, DOMSelectors.text);
+backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
